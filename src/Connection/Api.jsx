6f748bbeecb58b7b7edf5af465ee3e486cb20d11
Abdul Ahad <muhamedahamed251@gmail.com>
2025-07-16 19:59:56 +0500

    Update Api.jsx

--- src/Connection/Api.jsx
+++ src/Connection/Api.jsx
@@ -4,7 +4,7 @@ const isProduction = window.location.hostname !== "localhost";
 
 const axiosinstance = axios.create({
   baseURL: isProduction 
-    ? "https://varidate-back-production.up.railway.app/api" 
+    ? "https://varidate-backend-production-ae3e.up.railway.app/api" 
     : "http://localhost:3000/api",
   withCredentials: true,
   headers: {
